cd5335b57c0178d863a79413d8ffbc7e29677438
Lucien <luciferiizero@gmail.com>
2025-01-28 20:52:50 +0800

    Fixing the Structure, getting things to run

---
+++ src/main/java/com/mycompany/assignmentjava/Start.java
@@ -0,0 +1,17 @@
+package com.mycompany.assignmentjava;
+
+// import your classes 
+import com.mycompany.assignmentjava.Lucien.Login;
+import com.mycompany.assignmentjava.Saeed.*;
+import com.mycompany.assignmentjava.Shariq.*;
+
+import com.mycompany.assignmentjava.Lucien.Login;
+
+public class Start {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        // run whichever Jform you want from here:
+        Login login = new Login();
+        login.setVisible(true);
+    }
+}
