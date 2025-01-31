264816c368fd3fde793fb0ee53369763bc2aeb68
mayotomato <ahmedzakwaanshareef@gmail.com>
2025-01-31 08:33:00 +0800

    some comment changes

--- src/main/java/com/mycompany/assignmentjava/Zakwaan/Classes/Customer.java
+++ src/main/java/com/mycompany/assignmentjava/Zakwaan/Classes/Customer.java
@@ -4,10 +4,23 @@
  */
 package com.mycompany.assignmentjava.Zakwaan.Classes;
 
+import com.mycompany.assignmentjava.Utilites.User;
+
 /**
  *
  * @author zechn
  */
-public class Customer {
+public class Customer extends User{
+    //Attributes
+    private String address;
+    private String walletID;
+    
+    public Customer(String name, String email, String phone, String password, String address){
+        super(name, email, phone, password, "Customer");
+        
+        this.address = address;
+        this.walletID = ""; //new Wallet object
+        String poo = "poo";
+    }
     
 }
